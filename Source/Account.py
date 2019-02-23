2247c3fa920ec11cfb9965839bcc696a3edb458e
Matthew Vastarelli <mvast1@unh.newhaven.edu>
2019-02-23 12:50:22 -0500

    ABC class

--- Source/Account.py
+++ Source/Account.py
@@ -1,24 +1,40 @@
-class Account:
+from abc import ABC, abstractmethod
+
+
+class Account(ABC):
+    # this is the main abstract base account from which all accounts
+    # will inherits and define the abstract methods
+
+    # next id for a given account
     nextID = 1
 
     def __init__(self, uname, password, access=1):
+        # class attributes
         self.ownerID = __class__.nextID
         __class__.nextID += 1
         self.accessLevel = access
         self.userName = uname
         self.password = password
 
+        # super abstract class init
+        super.__init__()
+
+    @abstractmethod
     def log_in(self):
-        #login
+        pass
 
+    @abstractmethod
     def log_out(self):
-        # logout
+        pass
 
+    @abstractmethod
     def view(self):
-        #view
+        pass
 
+    @abstractmethod
     def edit(self):
-        #edit
+        pass
 
+    @abstractmethod
     def create_new(self):
-        #create new
+        pass
