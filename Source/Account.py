05969117dda1c536c28cea5078510a5824d8470d
Matthew Vastarelli <mvast1@unh.newhaven.edu>
2019-02-23 12:56:49 -0500

    private public

--- Source/Account.py
+++ Source/Account.py
@@ -10,31 +10,39 @@ class Account(ABC):
 
     def __init__(self, uname, password, access=1):
         # class attributes
+        # public
         self.ownerID = __class__.nextID
         __class__.nextID += 1
         self.accessLevel = access
-        self.userName = uname
-        self.password = password
+
+        # private
+        self.__userName = uname
+        self.__password = password
 
         # super abstract class init
         super.__init__()
 
+    # private
     @abstractmethod
-    def log_in(self):
+    def __log_in(self, user_name, password):
         pass
 
+    # private
     @abstractmethod
-    def log_out(self):
+    def __log_out(self):
         pass
 
+    # private
     @abstractmethod
-    def view(self):
+    def __view(self):
         pass
 
+    # private
     @abstractmethod
-    def edit(self):
+    def __edit(self):
         pass
 
+    # private
     @abstractmethod
-    def create_new(self):
+    def create_new(self, access_level):
         pass
