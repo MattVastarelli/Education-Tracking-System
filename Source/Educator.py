924e6d6e44037f9dde4bf91c8f5a393be01918f1
Katherine Bowers <k.bowers134@gmail.com>
2019-02-25 19:27:56 -0500

    Added Educator getters & setters, rearranged order of methods

--- Source/Educator.py
+++ Source/Educator.py
@@ -8,21 +8,52 @@ class Educator(Account):
     password = ""
     fname = ""
     lname = ""
-    homeAddress = ""
-    phoneNum = 0
-    email = ""
-    educatorID = int()
+    __homeAddress = ""
+    __phoneNum = 0
+    __email = ""
+    __educatorID = int()
     licenses = list()
-    courses = list()
-    prefSubjects = list()
+    __courses = list()
+    __prefSubjects = list()
     gradeLevels = list()
     currentInst = None
 
-    # methods for the Educator class
-
     # constructor
-    def __init__(self, username, password, access_level):
-        super().__init__(username, password, access_level)
+    def __init__(self, username, password):
+        super().__init__(username, password, 1)
+
+    # private
+    def __log_in(self, user_name, password):
+            pass
+
+    # private
+    def __log_out(self):
+            pass
+
+    # private
+    def __view(self):
+        pass
+
+    # private
+    def __edit(self):
+        pass
+
+    # private
+    def create_new(self, access_level):
+        pass
+
+    # ------------- Setters & Getters -------------------------
+    def get_name(self):
+        name = ' '.join(self.fname, self.lname)
+        return name
+
+    def get_personal_info(self):
+        return [self.__phoneNum, self.__email, self.__homeaddress]
+
+    def get_professional_info(self):
+        return [self.license, self.__educatorID, self.__prefSubject, self.gradeLevel, self.currentInstitution]
+
+    # Functions for actions done by an Educator within system
 
     # private function to replace a current grade with a new grade
     # done by pulling the table row specified by the passed in arguments, and updating the Grade attribute
@@ -55,24 +86,3 @@ class Educator(Account):
     # function to view students assigned to Educator's active sections
     def viewStudents(self):
         pass
-
-    # private
-    def __log_in(self, user_name, password):
-            pass
-
-    # private
-    def __log_out(self):
-            pass
-
-    # private
-    def __view(self):
-        pass
-
-    # private
-    def __edit(self):
-        pass
-
-    # private
-    def create_new(self, access_level):
-        pass
-
