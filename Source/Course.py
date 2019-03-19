75157c7b7be29316b849b83bf2f984a4ce86c1d4
Paul <peise1@unh.newhaven.edu>
2019-03-19 19:57:56 -0400

    Created Course and Subject files

---
+++ Source/Course.py
@@ -0,0 +1,11 @@
+class Course:
+    def __init__(self, subject):
+        self.subject = subject
+
+    def add_unit(self):
+        # creates a unit and returns it
+        return None
+
+    def add_section(self):
+        # creates a unit and returns it
+        return None
